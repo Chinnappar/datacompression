b7cba04784ce6109e0bf6a229e86233b58f5f19d
Chinnappar <112493795+Chinnappar@users.noreply.github.com>
2022-09-03 15:40:34 +0530

    Add files via upload

--- data_compression.py
+++ data_compression.py
@@ -25,34 +25,38 @@ import time
 import os
 import math
 import uuid
+pd.options.mode.chained_assignment = None  # default='warn'
 UUID = str(uuid.uuid1())
 
-file_mapping='mapping'+UUID+'.txt'
-file_compressed='compressed'+UUID+'.txt'
-zip_file_name = 'output'+UUID+'.zip'
+file_mapping='/mapping'+UUID+'.txt'
+file_compressed='/compressed'+UUID+'.txt'
+zip_file_name = '/output'+UUID+'.zip'
 
-pd.options.mode.chained_assignment = None  # default='warn'
+class compression:
+    def __init__(self):
+        #os.mkdir("/")
+        BASE_PATH='.'
 
 # ------------------------------------------------------------------------------
 # -- UDF's --
 # ------------------------------------------------------------------------------
-def convert_bytes(size):
-    for x in ['Bytes', 'KB', 'MB', 'GB', 'TB']:
-        if size < 1024.0:
-            return "%3.1f %s" % (size, x)
-        size /= 1024.0
+    def convert_bytes(self,size):
+        for x in ['Bytes', 'KB', 'MB', 'GB', 'TB']:
+            if size < 1024.0:
+                return "%3.1f %s" % (size, x)
+            size /= 1024.0
 
-def file_size(file):
-    if os.path.isfile(file):
-        file_info = os.stat(file)
-        return convert_bytes(file_info.st_size),file_info.st_size
+    def file_size(self,file):
+        if os.path.isfile(file):
+            file_info = os.stat(file)
+            return self.convert_bytes(file_info.st_size),file_info.st_size
 
-def listToDict(b):
-    s = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/'
-    if b=="b":
-        return {s[i]:i for i in range(len(s))}
-    else:
-        return {i:s[i] for i in range(len(s))}
+    def listToDict(self,b):
+        s = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/'
+        if b=="b":
+            return {s[i]:i for i in range(len(s))}
+        else:
+            return {i:s[i] for i in range(len(s))}
 
 #def base64_to_base10(b64dec):
 #    conversion_table = listToDict("b")
@@ -73,89 +77,88 @@ def listToDict(b):
 #        decimal = decimal // 64
 #    return b64dec
 
+    def base64_to_base10(self,b64dec,datatype=None):
+        conversion_table = self.listToDict("b")
 
-def base64_to_base10(b64dec,datatype=None):
-    conversion_table = listToDict("b")
+        if datatype == "f":
+            x=b64dec.split(".")[0]
+            y=b64dec.split(".")[1]
 
-    if datatype == "f":
-        x=b64dec.split(".")[0]
-        y=b64dec.split(".")[1]
+            x_dec=0
+            x_power = len(x)-1
+            for x_digit in x:
+                x_dec += conversion_table[x_digit]*64**x_power
+                x_power -= 1
 
-        x_dec=0
-        x_power = len(x)-1
-        for x_digit in x:
-            x_dec += conversion_table[x_digit]*64**x_power
-            x_power -= 1
+            y_dec,z,f=0,0,0
+            y_power = len(y)-1
+            for y_digit in y:
+                if y_digit=="0" and f==0:
+                    z=z+1
+                else:
+                    f=1
+                y_dec += conversion_table[y_digit]*64**y_power
+                y_power -= 1
 
-        y_dec,z,f=0,0,0
-        y_power = len(y)-1
-        for y_digit in y:
-            if y_digit=="0" and f==0:
-                z=z+1
-            else:
-                f=1
-            y_dec += conversion_table[y_digit]*64**y_power
-            y_power -= 1
+            return str(x_dec)+"."+str("").zfill(z)+str(y_dec)
+        else:
+            decimal = 0
+            power = len(b64dec) -1
+            for digit in b64dec:
+                decimal += conversion_table[digit]*64**power
+                power -= 1
+            return decimal
 
-        return str(x_dec)+"."+str("").zfill(z)+str(y_dec)
-    else:
-        decimal = 0
-        power = len(b64dec) -1
-        for digit in b64dec:
-            decimal += conversion_table[digit]*64**power
-            power -= 1
-        return decimal
+    def base10_to_base64(self,decimal,datatype=None):
+        conversion_table = self.listToDict("d")
 
-def base10_to_base64(decimal,datatype=None):
-    conversion_table = listToDict("d")
+        if datatype == "f":
+            x=int(str(decimal).split(".")[0])
+            y=str(decimal).split(".")[1].strip()
+            z=len(y)-len(str(int(y)))
+            y=int(y)
 
-    if datatype == "f":
-        x=int(str(decimal).split(".")[0])
-        y=str(decimal).split(".")[1].strip()
-        z=len(y)-len(str(int(y)))
-        y=int(y)
+            x_rem=0
+            x_b64dec=''
+            while(x > 0):
+                x_rem = x % 64
+                x_b64dec = conversion_table[x_rem] + x_b64dec
+                x = x // 64
 
-        x_rem=0
-        x_b64dec=''
-        while(x > 0):
-            x_rem = x % 64
-            x_b64dec = conversion_table[x_rem] + x_b64dec
-            x = x // 64
-
-        y_rem=0
-        y_b64dec=''
-        while(y > 0):
-            y_rem = y % 64
-            y_b64dec = conversion_table[y_rem] + y_b64dec
-            y = y // 64
-        return x_b64dec+"."+str("").zfill(z)+y_b64dec
-    else:
-        b64dec = ''
-        remainder=0
-        while(decimal > 0):
-            remainder = decimal % 64
-            b64dec = conversion_table[remainder] + b64dec
-            decimal = decimal // 64
-        return b64dec
+            y_rem=0
+            y_b64dec=''
+            while(y > 0):
+                y_rem = y % 64
+                y_b64dec = conversion_table[y_rem] + y_b64dec
+                y = y // 64
+            return x_b64dec+"."+str("").zfill(z)+y_b64dec
+        else:
+            b64dec = ''
+            remainder=0
+            while(decimal > 0):
+                remainder = decimal % 64
+                b64dec = conversion_table[remainder] + b64dec
+                decimal = decimal // 64
+            return b64dec
 # ------------------------------------------------------------------------------
 ## Function Name: file_compress
 ## Input : mapping and csvfile as a first parameter and 2nd = output file
 ## This is normal file compression function and this is similar to winzip/7z
 # ------------------------------------------------------------------------------
-def file_compress(inp_file_names, out_zip_file):
-    compression = zipfile.ZIP_DEFLATED
-    print(f" *** Input File name passed for zipping - {inp_file_names}")
-    print(f' *** out_zip_file is - {out_zip_file}')
-    zf = zipfile.ZipFile(out_zip_file, mode="w")
+    def file_compress(self,inp_file_names, out_zip_file):
+        compression = zipfile.ZIP_DEFLATED
+        print(f" *** Input File name passed for zipping - {inp_file_names}")
+        print(f' *** out_zip_file is - {out_zip_file}')
+        zf = zipfile.ZipFile(out_zip_file, mode="w")
 
-    try:
-        for file_to_write in inp_file_names:
-            print(f' *** Processing file {file_to_write}')
-            zf.write(file_to_write, file_to_write, compress_type=compression)
-    except FileNotFoundError as e:
-        print(f' *** Exception occurred during zip process - {e}')
-    finally:
-        zf.close()
+        try:
+            for file_to_write in inp_file_names:
+                print(f' *** Processing file {file_to_write}')
+                zf.write(file_to_write, file_to_write, compress_type=compression)
+        except FileNotFoundError as e:
+            print(f' *** Exception occurred during zip process - {e}')
+        finally:
+            zf.close()
 
 # ------------------------------------------------------------------------------
 ## Function Name: csvfile_compression and Input : Csv file file full path
@@ -167,83 +170,88 @@ def file_compress(inp_file_names, out_zip_file):
 ## 5.Concatenate all the rows and make it single text [completed]
 ## final file will be compressed with normal compression function like winzip
 # ------------------------------------------------------------------------------
-def csvfile_compression(filepath):
-    try:
-        train_df=pd.read_csv(filepath)
-        #msg="Source file's size:"+str(train_df.size)
-        df_map=[]
-        df_col={col:len(train_df[col].unique()) for col in train_df.columns}
-        df_dt=[train_df[col].dtypes for col in train_df.columns ]
-        key_srtby=""
+    def csvfile_compression(self,filepath):
+        try:
+            train_df=pd.read_csv(filepath)
+            #msg="Source file's size:"+str(train_df.size)
+            df_map=[]
+            df_col={col:len(train_df[col].unique()) for col in train_df.columns}
+            df_dt=[train_df[col].dtypes for col in train_df.columns ]
+            key_srtby=""
 
-        for col in train_df.columns:
-            col_len=len(train_df[col].unique())
-            print("Column Name:",col,"|Unique Cnt:",col_len,"|DataType:",train_df[col].dtypes,"| DateTime:",datetime.datetime.now())
-            if col_len < 3000 :
-                df_unique=train_df[col].unique()
-                s=",".join(map(str,df_unique))
-                train_df[col] = train_df[col].replace(df_unique[0:int(col_len/2)],[a for a in range(int(col_len/2))])
-                train_df[col] = train_df[col].replace(df_unique[int(col_len/2)-1:col_len],[a for a in range(int(col_len/2)-1,col_len)])
-                train_df[col] = train_df[col].apply(lambda x: x if np.isnan(x) else base10_to_base64(int(x)))
-            elif train_df[col].dtypes=='int64':
-                #print("Base64 Conversion...",train_df[col].dtypes)
-                train_df[col] = train_df[col].apply(lambda x: x if np.isnan(x) else base10_to_base64(int(x)))
-                s="b"
-            elif train_df[col].dtypes=='object':
-                try:
-                    train_df[col] = pd.to_datetime(train_df[col]).view(int) // 10 ** 9
-                    train_df[col] = train_df[col].apply(lambda x: x if np.isnan(x) else base10_to_base64(int(x)))
-                    #print("Date Column:",col)
-                    s="d"
-                except (ParserError,ValueError):
-                    pass
-            elif train_df[col].dtypes=='float64':
-                train_df[col] = train_df[col].apply(lambda x: x if np.isnan(x) else base10_to_base64(x,"f"))
-                s="f"
-            else: # or train_df[col].dtypes=='float64':
-                s="n"
-            df_map.append(s)
-        df_map.append(str(df_col))
-        df_map.append(str(df_dt))
+            for col in train_df.columns:
+                col_len=len(train_df[col].unique())
+                print("Column Name:",col,"|Unique Cnt:",col_len,"|DataType:",train_df[col].dtypes,"| DateTime:",datetime.datetime.now())
+                if col_len < 3000 :
+                    df_unique=train_df[col].unique()
+                    s=",".join(map(str,df_unique))
+                    train_df[col] = train_df[col].replace(df_unique[0:int(col_len/2)],[a for a in range(int(col_len/2))])
+                    train_df[col] = train_df[col].replace(df_unique[int(col_len/2)-1:col_len],[a for a in range(int(col_len/2)-1,col_len)])
+                    train_df[col] = train_df[col].apply(lambda x: x if np.isnan(x) else self.base10_to_base64(int(x)))
+                elif train_df[col].dtypes=='int64':
+                    #print("Base64 Conversion...",train_df[col].dtypes)
+                    train_df[col] = train_df[col].apply(lambda x: x if np.isnan(x) else self.base10_to_base64(int(x)))
+                    s="b"
+                elif train_df[col].dtypes=='object':
+                    try:
+                        train_df[col] = pd.to_datetime(train_df[col]).view(int) // 10 ** 9
+                        train_df[col] = train_df[col].apply(lambda x: x if np.isnan(x) else self.base10_to_base64(int(x)))
+                        #print("Date Column:",col)
+                        s="d"
+                    except (ParserError,ValueError):
+                        pass
+                elif train_df[col].dtypes=='float64':
+                    train_df[col] = train_df[col].apply(lambda x: x if np.isnan(x) else self.base10_to_base64(x,"f"))
+                    s="f"
+                else: # or train_df[col].dtypes=='float64':
+                    s="n"
+                df_map.append(s)
+            df_map.append(str(df_col))
+            df_map.append(str(df_dt))
 
-        #srtby=min(df_col, key=df_col.get)
-        #train_df=train_df.sort_values(by=srtby)
-        #df_map.append("s-"+srtby)
+            #srtby=min(df_col, key=df_col.get)
+            #train_df=train_df.sort_values(by=srtby)
+            #df_map.append("s-"+srtby)
 
-        print(df_col)
-        print(train_df.head())
+            print(df_col)
+            print(train_df.head())
 
-        #file_mapping='mapping.txt'
-        with open(file_mapping, 'w') as f:
-            f.write("|".join(df_map))
+            file_mapping='mapping.txt'
+            with open(file_mapping, 'w') as f:
+                f.write("|".join(df_map))
 
-        df_comp=[]
-        for col in train_df.columns:
-            s=",".join(map(str,train_df[col]))
-            df_comp.append(s)
+            df_comp=[]
+            for col in train_df.columns:
+                s=",".join(map(str,train_df[col]))
+                df_comp.append(s)
 
-        with open(file_compressed, 'w') as f:
-            f.write("|".join(df_comp))
+            file_compressed='compressed.txt'
+            with open(file_compressed, 'w') as f:
+                f.write("|".join(df_comp))
 
-        #df_final="|".join(df_comp)
-        #df = pd.DataFrame(list(df_final), columns = ['compressed'])
-        #msg=msg+" After Compressed File Info:"+str(len(df_final))+":"+str(df.size)
-        msg="Data compression is completed! Please download the zip file."
-        file_name_list = [file_mapping, file_compressed]
-        #zip_file_name = filepath+".zip"
+            #df_final="|".join(df_comp)
+            #df = pd.DataFrame(list(df_final), columns = ['compressed'])
+            #msg=msg+" After Compressed File Info:"+str(len(df_final))+":"+str(df.size)
+            msg="Data compression is completed! Please download the zip file."
+            file_name_list = [file_mapping, file_compressed]
+            #zip_file_name = filepath+".zip"
 
-        #zip_file_name = "output.zip"
-        file_compress(file_name_list, zip_file_name)
+            zip_file_name = "output.zip"
+            self.file_compress(file_name_list, zip_file_name)
 
-        return msg,zip_file_name,train_df
-    except Exception as ex:
-            print("Error:"+str(ex))
-            df=[]
-            df.append(ex)
-            return "failed"+str(ex),df
+            return msg,zip_file_name,train_df
+        except Exception as ex:
+                print("Error:"+str(ex))
+                df=[]
+                df.append(ex)
+                return "failed"+str(ex),df
+
+
+
 
 def s_ui():
     try:
+        comp=compression()
         st.set_page_config(layout = "wide")
         st.title("Data Compression")
         st.info("Developed by Chinnappar & Team (R-AI)")
@@ -263,12 +271,12 @@ def s_ui():
 
         if st.button("Test"):
             test_file="training_data_sales_10k.csv"
-            msg,output,train_df=csvfile_compression(test_file)
+            msg,output,train_df=comp.csvfile_compression(test_file)
             st.info("Data compression is completed for test file. Please find the details below...")
-            ftest_size,test_size=file_size(test_file)
-            ftmap_size,tmap_size=file_size("mapping.txt")
-            ftcomp_size,tcomp_size=file_size("compressed.txt")
-            ftzip_size,tzip_size=file_size(output)
+            ftest_size,test_size=comp.file_size(test_file)
+            ftmap_size,tmap_size=comp.file_size("mapping.txt")
+            ftcomp_size,tcomp_size=comp.file_size("compressed.txt")
+            ftzip_size,tzip_size=comp.file_size(output)
             tnumber="{:.2%}".format((test_size-(tcomp_size+tmap_size))/test_size)
 
             df_test=pd.read_csv(test_file)
@@ -298,12 +306,12 @@ def s_ui():
         csv_file = st.file_uploader("Please upload your own csv file", type=['csv'], accept_multiple_files=False)
 
         if csv_file is not None:
-            msg,output,train_df=csvfile_compression(csv_file)
+            msg,output,train_df=comp.csvfile_compression(csv_file)
             st.info(msg)
             csv_size=csv_file.size
-            fmap_size,map_size=file_size("mapping.txt")
-            fcomp_size,comp_size=file_size("compressed.txt")
-            fzip_size,zip_size=file_size(output)
+            fmap_size,map_size=comp.file_size("mapping.txt")
+            fcomp_size,comp_size=comp.file_size("compressed.txt")
+            fzip_size,zip_size=comp.file_size(output)
             number="{:.2%}".format((csv_size-(comp_size+map_size))/csv_size)
 
             with st.expander("ℹ️ - Compressed - Sample Data:", expanded=True):
@@ -312,7 +320,7 @@ def s_ui():
             with st.expander("ℹ️ - Results:", expanded=True):
                 st.write(
                     f'''
-                 -  Uploaded File Details- File Name: {csv_file.name} File Type: {csv_file.type} File Size: {convert_bytes(csv_size)}
+                 -  Uploaded File Details- File Name: {csv_file.name} File Type: {csv_file.type} File Size: {comp.convert_bytes(csv_size)}
                  -  Size of mapping file which is used for decompression: {fmap_size}
                  -  Size of compression csv file: {fcomp_size}
                  -  Size of zipped file for above two: {fzip_size}
@@ -337,16 +345,16 @@ def s_ui():
 
     except Exception as ex:
         st.write("Failed!:... "+str(ex))
-
 # ------------------------------------------------------------------------------
 # Call main function using csv file as a input
 # This main function is used for testing purpose in local
 # ------------------------------------------------------------------------------
 if __name__ == "__main__":
     try:
+        comp=compression()
         print("Started - DateTime:",datetime.datetime.now())
-        #csvfile_compression('training_data_sales_10k.csv')
-        s_ui()
+        comp.csvfile_compression('training_data_sales_10k.csv')
+        #s_ui()
         print("compression is completed...")
         print("End - DateTime:",datetime.datetime.now())
 
